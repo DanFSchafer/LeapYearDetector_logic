069975363eada25c73acff7616a53ad22eb004ff
Dan Schafer and Gray Hildreth <dan@>
2016-06-20 19:40:01 -0700

    Finish up logic for leapYear().

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
   return false;
   }
-
 };
 
 
